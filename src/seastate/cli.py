54a48e78f0d60a4ca71ce93e329f1c29a47e28d8
Bror Jonsson <brorfred@gmail.com>
2026-01-20 21:32:49 -0500

    Start working on cli

---
+++ src/seastate/cli.py
@@ -0,0 +1,50 @@
+
+import typer
+from typing import Annotated
+
+
+import seastate
+
+app = typer.Typer()
+
+@app.command()
+def update(
+    env: Annotated[str, typer.Option(help="Environment used in settings file.")] = "DEFAULT",
+    sync: Annotated[bool, typer.Option(help="Sync tiles to remote.")] = True,
+
+):
+    """
+    Update tiles with yesterday's and today's fields"
+    """
+
+    if env.lower() != "default":
+        seastate.config.change_env(env)
+    print(env)
+    #seastate.today(force=False, sync=False)
+    #seastate.yesterday(force=True, sync=sync)
+    #
+
+@app.callback()
+def callback():
+    """
+    A cruise support system converting geophysical fields to slippy tiles.
+    Currently SST, SSH, and Chl from Copernicus are available.
+    """
+
+
+
+
+@app.command()
+def shoot():
+    """
+    Shoot the portal gun
+    """
+    typer.echo("Shooting portal gun")
+
+
+@app.command()
+def load():
+    """
+    Load the portal gun
+    """
+    typer.echo("Loading portal gun")
